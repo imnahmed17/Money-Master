04adc81503c2064f1942b53c323e2d1ac9108a74
Noman Ahmed Ashik <48390490+imnahmed17@users.noreply.github.com>
2023-03-08 16:11:53 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,19 +16,19 @@ const calculateExpense = () => {
   rent < 0 || isNaN(rent) == true || 
   clothes < 0 || isNaN(clothes) == true) {
     alert("Inputs must be positive numbers");
+	  return;
+  } 
+  // calculate expense
+  const expense = food + rent + clothes;
+  // calculate balance
+  const balance = income - expense;
+  // validate income
+  if (expense > income) {
+    alert("Expenses cannot be more than income");
   } else {
-    // calculate expense
-    const expense = food + rent + clothes;
-    // calculate balance
-    const balance = income - expense;
-    // validate income
-    if (expense > income) {
-      alert("Expenses cannot be more than income");
-    } else {
-      // view total expense and balance
-      document.getElementById("total-expense").innerText = expense;
-      document.getElementById("balance").innerText = balance;
-    }
+    // view total expense and balance
+    document.getElementById("total-expense").innerText = expense;
+    document.getElementById("balance").innerText = balance;
   }
 };
 
@@ -40,19 +40,19 @@ const calculateSavings = () => {
   if (income < 0 || isNaN(income) == true || 
   savePercentage < 0 || isNaN(savePercentage) == true) {
     alert("Provide positive saving value");
+	  return;
+  }
+  // calculate saving amount
+  const savingAmount = savePercentage / 100 * income;
+  // calculate remaining balance
+  const balance = document.getElementById("balance").innerText;
+  const remainingBalance = parseInt(balance) - savingAmount;
+  // validate saving amount
+  if (savingAmount > balance) {
+    alert("SavingAmount is greater than balance");
   } else {
-    // calculate saving amount
-    const savingAmount = savePercentage / 100 * income;
-    // calculate remaining balance
-    const balance = document.getElementById("balance").innerText;
-    const remainingBalance = parseInt(balance) - savingAmount;
-    // validate saving amount
-    if (savingAmount > balance) {
-      alert("SavingAmount is greater than balance");
-    } else {
-      // view saving amount and remaining balance
-      document.getElementById("saving-amount").innerText = savingAmount;
-      document.getElementById("remaining-balance").innerText = remainingBalance;
-    }
+    // view saving amount and remaining balance
+    document.getElementById("saving-amount").innerText = savingAmount;
+    document.getElementById("remaining-balance").innerText = remainingBalance;
   }
 };
